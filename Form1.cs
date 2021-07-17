43422dd92d7c97d55b160dfaea1d7239743a8958
ae <ae@ACER>
2021-07-17 17:56:57 +0800

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace problem1_1.calapini_bscpe_1_5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Console.Beep(800, 200);
+        }
+    }
+}
